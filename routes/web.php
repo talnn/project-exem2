5def72437e9bef93232bf789a0e36de0770c4b40
hacongmanh <55304634+hacongmanh@users.noreply.github.com>
2020-07-23 23:31:34 +0700

    update layout

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Admin');
+});
+Route::get('/user.blade',function (){
+    return view('user');
 });
